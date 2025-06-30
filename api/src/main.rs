b623c1e5883022c4fe9236de7ac2849a874e857c
Krishna Mohan <krishanmohank974@gmail.com>
2025-06-30 18:33:06 +0530

    Init store package

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,6 +3,7 @@ use poem::{
 };
 
 use crate::{request_input::CreateWebsteInput, request_output::CreateWebsteOutput};
+use store::Store;
 pub mod request_input;
 pub mod request_output;
 
@@ -10,6 +11,8 @@ pub mod request_output;
 #[handler]
 // path is used for params (/:sdfjladsfj)
 fn get_website(Path(name): Path<String>) -> String {
+    let s = Store{};
+    s.create_user();
     format!("hello: {name}")
 }
 
@@ -17,21 +20,22 @@ fn get_website(Path(name): Path<String>) -> String {
 // Post Endpoint for creating website
 // In TypeScript, this would be like:
 // function createWebsite(data: { url: string }): { id: string }
-//
-// The Json<T> wrapper in Rust is similar to how Express/Koa 
+
 // automatically parse JSON request bodies into JavaScript objects.
 // It handles deserialization of the incoming JSON into our CreateWebsteInput struct
 // and serialization of our CreateWebsteOutput struct back into JSON.
 #[handler]
 fn create_website(Json(data): Json<CreateWebsteInput>) -> Json<CreateWebsteOutput> {
     let url = data.url;
-    let resposne =  CreateWebsteOutput{
-        id: url
-    };
+    
+    let s = Store{};
+    let id = s.create_website();
     // persist it in db 
     // sqlx => postgreqs with raw sql
     // diesel => prisma (orm)
-    
+    let resposne =  CreateWebsteOutput{
+        id
+    };
     Json(resposne)
 }
 
